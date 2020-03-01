ceb19ddd4ad4b8f30396fa49872928887afa415e
Dave Van Fleet <dvfleet413@gmail.com>
2020-02-29 23:28:22 -0500

    finished product controller and added button links on all pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 require "./config/environment"
 require "./app/models/product"
+require 'pry'
 
 class ProductsController < Sinatra::Base
 
@@ -11,37 +12,57 @@ class ProductsController < Sinatra::Base
 
   #Create
   get '/products/new' do
-
+    @product = Product.new
+    erb :'products/new'
   end
 
   post '/products' do
-
+    @product = Product.create({name: params[:name],
+                               price: params[:price].to_f,
+                               quantity: params[:quantity],
+                               user_id: session[:user_id]})
+    redirect "/products/#{@product.id}"
   end
 
 
   #Read
   get '/products' do
+    @products = Product.all.select{|product| product.user_id == session[:user_id]}
+    sql = <<-SQL
+      SELECT SUM(products.price * products.quantity) FROM products
+      WHERE products.user_id = #{session[:user_id]}
+    SQL
+    @total = ActiveRecord::Base.connection.execute(sql)[0]['sum']
     erb :'products/index'
   end
 
   get '/products/:id' do
-
+    @product = Product.find(params[:id])
+    erb :'products/show'
   end
 
 
   #Update
   get '/products/:id/edit' do
-
+    @product = Product.find(params[:id])
+    erb :'products/edit'
   end
 
   patch '/products/:id' do
-
+    @product = Product.find(params[:id])
+    @product.name = params[:name]
+    @product.price= params[:price]
+    @product.quantity = params[:quantity]
+    @product.save
+    redirect "/products/#{@product.id}"
   end
 
 
   #Delete
   delete '/products/:id' do
-
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect '/products'
   end
 
 
--- config.ru
+++ config.ru
@@ -2,7 +2,10 @@ require 'sinatra/base'
 
 require './app/controllers/application_controller'
 require './app/controllers/users_controller'
+require './app/controllers/products_controller'
 
+use Rack::MethodOverride
 
 use UsersController
+use ProductsController
 run ApplicationController
